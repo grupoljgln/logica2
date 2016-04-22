f5c383edef2805ad09ae74d4dcb4a89873a71ec2
eguimaraes15 <eguimaraes@unifor.br>
2016-04-22 10:11:19 -0300

    Update Main.java
    Inserindo o comentario para identificacao da questao.

--- src/br/unifor/cct/cc/logica/pratica/Main.java
+++ src/br/unifor/cct/cc/logica/pratica/Main.java
@@ -9,6 +9,9 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+	// Q01 - Faça um programa que declare uma estrutura que represente os dados de 
+	// acordo com a tabela a seguir.
 
 	}
 
